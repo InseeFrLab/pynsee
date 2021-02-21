8afa4795fe62a6190758d684c2070619441bde91
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-21 23:19:56 +0100

    website doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -33,9 +33,11 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
 extensions = [
+     "sphinx_rtd_theme"
 ]
-
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
