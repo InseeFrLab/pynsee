de9d15c0455aac71ae46033cd18c5c34958e1238
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-21 23:29:52 +0100

    doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ import sphinx_rtd_theme
 extensions = [
      "sphinx_rtd_theme"
 ]
-html_theme = "sphinx_rtd_theme"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -52,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
