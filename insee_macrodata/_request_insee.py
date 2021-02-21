f9080a33ddb2b1d14baa4d9eb078e8621094ba9d
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-21 18:06:11 +0100

    request_insee nested try catch to handle token and urls

--- insee_macrodata/_request_insee.py
+++ insee_macrodata/_request_insee.py
@@ -6,9 +6,10 @@ def _warning_api_success():
 
 @lru_cache(maxsize=None)
 def _warning_api_failure():
-    msg1 = "\n!!! Warning : check your subscription to api.insee.fr"
-    msg2 = "Due to an error the sdmx service is used instead of the api!!!\n"
-    print(msg1 + "\n" + msg2)
+    msg1 = "\n!!! Error : check your subscription to api.insee.fr"
+    #msg2 = "Due to an error the sdmx service is used instead of the api!!!\n"
+    #print(msg1 + "\n" + msg2)
+    print(msg1 + "\n" )
 
 @lru_cache(maxsize=None)
 def _warning_no_token():
@@ -16,32 +17,78 @@ def _warning_no_token():
     msg2 = "The sdmx service you are using may be deprecated !!!\n"
     print(msg1 + "\n" + msg2)   
 
-def _request_insee(api_url, sdmx_url=None):
+def _request_insee(api_url=None, sdmx_url=None):
 
     import os
     import requests
     from ._get_token import _get_token
-
-    proxies = {'http': os.environ.get('http'),
-               'https': os.environ.get('https')}
-        
-    token = _get_token()
-  
-    if not token is None:
-        headers = {'Accept': 'application/xml',
-                    'Authorization': 'Bearer ' + token}
-        try:
-            _warning_api_success()
-            results = requests.get(api_url, proxies = proxies, headers=headers)
-        except:
-            _warning_api_failure()
-            results = requests.get(sdmx_url, proxies = proxies)
-    else:
-        if not sdmx_url is None:
-            _warning_no_token()         
-            results = requests.get(sdmx_url, proxies = proxies)
-        else:
-            print("!!! Error : token missing, check your keys correspond to your api.insee.fr account credentials")
-
     
-    return(results)
+    # sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/001688370"
+    # api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/001688370"
+   
+
+    try:
+        proxies = {'http': os.environ['http'],
+                   'https': os.environ['https']}
+    except:
+        proxies = {'http': '','https': ''}
+    
+    # if api_url is provided, it is used first, 
+    # and the sdmx url is used as a backup in two cases
+    # 1- when the token is missing 
+    # 2- if the api request fails
+    
+    # if api url is missing sdmx url is used
+    
+    if not api_url is None:
+        
+        try:
+            token = os.environ['insee_token']
+        except:
+            token = _get_token()
+      
+        if not token is None:
+            headers = {'Accept': 'application/xml',
+                        'Authorization': 'Bearer ' + token}
+            
+            results = requests.get(api_url, proxies = proxies, headers=headers)
+            
+            if results.status_code != 200:
+                
+                print("!!! Wrong query or api.insee.fr error\n Please check credentials")
+                
+                if not sdmx_url is None:
+                    
+                    results = requests.get(sdmx_url, proxies = proxies)
+                    print("!!! SDMX web service used instead of API !!!")
+                    
+                    if results.status_code != 200:                        
+                        raise ValueError(results.text + '\n' + sdmx_url)                    
+                                                  
+        else:
+            # token is None
+            
+            msg = "!!! Token missing, please check your keys on api.insee.fr\n"
+            if not sdmx_url is None:
+                #_warning_no_token()     
+                print(msg)
+                results = requests.get(sdmx_url, proxies = proxies)
+                print("SDMX web service used instead of API")
+                if results.status_code != 200:
+                    raise ValueError(results.text + '\n' + sdmx_url)
+                
+            else:
+                ValueError(msg)
+    else:
+        #api_url is None
+        if not sdmx_url is None:
+            results = requests.get(sdmx_url, proxies = proxies)
+            
+            if results.status_code != 200:
+                    raise ValueError(results.text + '\n' + sdmx_url) 
+        else:
+            raise ValueError("!!! Error : urls are missing")
+    try:
+        return(results)
+    except:
+        ValueError("Error")
