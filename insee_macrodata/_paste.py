fe93d03a2321d75c30d0a038d61594f2baa8df17
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-22 12:14:21 +0100

    weird line in _paste

--- insee_macrodata/_paste.py
+++ insee_macrodata/_paste.py
@@ -12,5 +12,6 @@ def _reduce_concat(x, sep=""):
 def _paste(*lists, sep=" ", collapse=None):
     result = map(lambda x: _reduce_concat(x, sep=sep), zip(*lists))
     if collapse is not None:
-          return _reduce_concat(result, sep=collapse)
-          return list(result)
+        return _reduce_concat(result, sep=collapse)
+    else:
+        return list(result)
