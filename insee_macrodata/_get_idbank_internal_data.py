798a57dcf6bc8c1fdb7b5df28d04d2b5ee46cb12
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-24 00:46:16 +0100

    try bug fix

--- insee_macrodata/_get_idbank_internal_data.py
+++ insee_macrodata/_get_idbank_internal_data.py
@@ -5,15 +5,42 @@ Created on Sun Feb 14 21:15:52 2021
 @author: XLAPDO
 """
 
+import zipfile
 import pkg_resources
 import pandas as pd
-from functools import lru_cache
+import os
+    
+#from functools import lru_cache
 
-@lru_cache(maxsize=None)
+#@lru_cache(maxsize=None)
 def _get_idbank_internal_data():
-    data_file = pkg_resources.resource_stream(__name__, 'data/idbank_list_internal.csv')
-    idbank_list = pd.read_csv(data_file, encoding = 'latin-1',
-                              quotechar='"', sep=',', header = [0], index_col = 0,
-                              dtype=str, usecols = [0,1,2,538,539])
-    idbank_list = idbank_list[["nomflow", "idbank", "cleFlow", "title_fr", "title_en"]]
+    
+    from ._create_insee_folder import _create_insee_folder
+    from ._hash import _hash
+    
+    insee_folder = _create_insee_folder()
+    
+    data_file = insee_folder + '/' + 'idbank_list_internal.csv'
+    data_final_file = insee_folder + '/' + _hash('idbank_list_internal_final')
+    zip_file = pkg_resources.resource_stream(__name__, 'data/idbank_list_internal.zip')
+    
+    if not os.path.exists(data_final_file):
+       
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(insee_folder)            
+            
+        idbank_list = pd.read_csv(data_file, encoding = 'latin-1',
+                                  quotechar='"', sep=',', dtype=str, usecols = [0,1,2,538,539])
+        
+        os.remove(data_file)
+        idbank_list.to_pickle(data_final_file)
+    else:        
+        try:            
+            idbank_list = pd.read_pickle(data_final_file)
+        except:
+            os.remove(data_final_file)
+            idbank_list = _get_idbank_internal_data()
+        #else:
+        #   print('Cached data used')
+    
     return idbank_list
