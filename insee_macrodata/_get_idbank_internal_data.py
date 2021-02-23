057dc3b3bf1d5a09ebbdc2930b6a7ab104cd975b
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-23 20:06:14 +0100

    try bug fix

--- insee_macrodata/_get_idbank_internal_data.py
+++ insee_macrodata/_get_idbank_internal_data.py
@@ -13,7 +13,7 @@ from functools import lru_cache
 def _get_idbank_internal_data():
     data_file = pkg_resources.resource_stream(__name__, 'data/idbank_list_internal.csv')
     idbank_list = pd.read_csv(data_file, encoding = 'latin-1',
-                              quotechar='"', sep=',', header = [0],
+                              quotechar='"', sep=',', header = [0], index_col = 0,
                               dtype=str, usecols = [0,1,2,538,539])
     idbank_list = idbank_list[["nomflow", "idbank", "cleFlow", "title_fr", "title_en"]]
     return idbank_list
