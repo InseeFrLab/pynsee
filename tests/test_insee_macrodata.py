be430c80497f0ea2459ba273d3e8e3c5d2994416
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-23 18:11:19 +0100

    test _get_idbank_internal_data

--- tests/test_insee_macrodata.py
+++ tests/test_insee_macrodata.py
@@ -94,10 +94,14 @@ class TestFunction(TestCase):
     #     test1 = isinstance(search_all, pd.DataFrame)
     #     test2 = isinstance(search_paris, pd.DataFrame)
     #     self.assertTrue(test1 & test2)
+    def test__get_idbank_internal_data(self):
+        df = imac._get_idbank_internal_data()        
+        test = isinstance(df, pd.DataFrame)
+        self.assertTrue(test)
 
     def test__get_idbank_internal_data_harmonized(self):
-        search_all = imac._get_idbank_internal_data_harmonized()        
-        test = isinstance(search_all, pd.DataFrame)
+        df = imac._get_idbank_internal_data_harmonized()        
+        test = isinstance(df, pd.DataFrame)
         self.assertTrue(test)
 
     # def test__download_idbank_list(self):
