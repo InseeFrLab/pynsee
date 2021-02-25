ffbeff9c85632e32cf9df10d96f272d0c953c23e
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-25 22:44:14 +0100

    use envir key first, then query insee

--- insee_macrodata/_get_token.py
+++ insee_macrodata/_get_token.py
@@ -4,14 +4,23 @@ from functools import lru_cache
 def _get_token():
     import os
     from api_insee import ApiInsee
-    try:       
-        api = ApiInsee(
-            key = os.environ.get('insee_key'),
-            secret = os.environ.get('insee_secret')
-        )
-        token = api.auth.token.access_token
-    except:
-        token = None
+    
+    from ._get_envir_token import _get_envir_token
+
+    token_envir = _get_envir_token()
+
+    if token_envir is None:
+        try:       
+            api = ApiInsee(
+                key = os.environ.get('insee_key'),
+                secret = os.environ.get('insee_secret')
+            )
+            token = api.auth.token.access_token
+        except:
+            token = None
+    else:
+        token = token_envir
+
     return(token)
 
 
