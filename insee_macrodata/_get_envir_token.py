ef43f803d17010e562f2983ed6566c09da11029f
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-02-25 22:43:44 +0100

    test envir key in a function whose result is cached

---
+++ insee_macrodata/_get_envir_token.py
@@ -0,0 +1,24 @@
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def _get_envir_token():
+    import os, requests
+
+    try:
+        proxies = {'http': os.environ['http_proxy'],
+                   'https': os.environ['http_proxy']}
+    except:
+        proxies = {'http': '','https': ''}
+
+    try:
+        token = os.environ['insee_token']
+        headers = {'Accept': 'application/xml',
+                    'Authorization': 'Bearer ' + token}
+        url_test = "https://api.insee.fr/series/BDM/V1/data/CLIMAT-AFFAIRES"
+        request_test = requests.get(url_test, proxies = proxies, headers=headers)
+        if request_test.status_code != 200:
+                raise ValueError("Token from python environment is not")   
+    except:
+        token = None
+    return(token)
