10a773b42fa1db95e71c6cd2359d675158de8c8f
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-10 23:49:11 +0100

    request in utils

--- pynsee/local/_get_geo_relation.py
+++ pynsee/local/_get_geo_relation.py
@@ -20,7 +20,7 @@ def _get_geo_relation(geo, code, relation):
     import xml.etree.ElementTree as ET
     from tqdm import trange
 
-    from ._request_insee import _request_insee
+    from pynsee.utils._request_insee import _request_insee
 
     api_url = 'https://api.insee.fr/metadonnees/V1/geo/' + geo + '/' + code + '/' + relation
     
