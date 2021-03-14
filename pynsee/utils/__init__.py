a639b75ddb5a49813db0567d5d4e11130708c9db
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-14 14:25:53 +0100

    clear_all_cache in init

--- pynsee/utils/__init__.py
+++ pynsee/utils/__init__.py
@@ -6,3 +6,5 @@ from ._get_envir_token import _get_envir_token
 from ._clean_insee_folder import _clean_insee_folder
 from ._create_insee_folder import _create_insee_folder
 from ._request_insee import _request_insee
+
+from .clear_all_cache import clear_all_cache
