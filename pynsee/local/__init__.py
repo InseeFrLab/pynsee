1bc03abf29184913127342971d756faaeaf18fe8
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-14 17:30:54 +0100

    functions in init for local

--- pynsee/local/__init__.py
+++ pynsee/local/__init__.py
@@ -1,6 +1,6 @@
 
 
 from .get_geo_list import get_geo_list
-from .get_map import get_map
-#to be deleted
 from .get_map_list import get_map_list
+from .get_map_link import get_map_link
+from .get_map import get_map
