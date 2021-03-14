263c0787ede85e9b59e96af2f3409d5b2dd51994
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-15 00:01:21 +0100

    add random value to map

--- pynsee/local/get_map.py
+++ pynsee/local/get_map.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 def get_map(geo):
+
+    # test
+    # import matplotlib
+    # import descartes
+    # map = get_map('communes')
+    # map.plot(column='value')
     
     import geopandas as gpd
+    import numpy as np
     
     from pynsee.local.get_map_link import get_map_link
     
     map = gpd.read_file(get_map_link(geo))
 
+    map['value'] = np.random.randint(1, 10, map.shape[0])
+   
     return(map)
