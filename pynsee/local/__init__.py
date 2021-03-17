29dad5b466037ec76025bba344ea9a6cef366c4c
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-17 12:35:21 +0100

    add functions to init

--- pynsee/local/__init__.py
+++ pynsee/local/__init__.py
@@ -4,3 +4,7 @@ from .get_geo_list import get_geo_list
 from .get_map_list import get_map_list
 from .get_map_link import get_map_link
 from .get_map import get_map
+
+from ._get_local import _get_local
+from ._get_nivgeo import _get_nivgeo
+from ._get_local_metadata import _get_local_metadata
