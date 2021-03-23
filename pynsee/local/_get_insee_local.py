00ebac648f7fd2f0e2588af6250b0a366fc1a104
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-23 16:20:16 +0100

    bug fix and use list of geo code

--- pynsee/local/_get_insee_local.py
+++ pynsee/local/_get_insee_local.py
@@ -1,75 +1,92 @@
 # -*- coding: utf-8 -*-
 
-def _get_insee_local(variables, dataset, codegeo, geo):
+def _get_insee_local(variables, dataset, geo, geocodes):
     
     from pynsee.utils._request_insee import _request_insee
     from pynsee.utils._paste import _paste
     
+    from tqdm import trange
     import pandas as pd
+    import numpy as np
     
-#    variables = 'AGESCOL-SEXE-ETUD';dataset = 'GEO2019RP2011';codegeo = '91';geo = 'DEP'
+#    variables = 'AGESCOL-SEXE-ETUD';dataset = 'GEO2019RP2011';geocodes = ['91','92', '976'];geo = 'DEP'
     
-    if type(variables) == list:
-        variables = _paste(variables, collapse = '-')
+    list_data_all = []
+    
+    for cdg in trange(len(geocodes), desc = "Getting data"): 
         
-    count_sep = variables.count('-')
-    modalite = '.all' * (count_sep+1)
-    
-    link = 'https://api.insee.fr/donnees-locales/V0.1/donnees/'
-    link = link + 'geo-{}@{}/{}-{}{}'.format(variables, dataset, geo, codegeo, modalite)
-    
-    request = _request_insee(api_url = link, file_format = 'application/json;charset=utf-8')
-    
-    data_request = request.json()
-    
-    Cellule = data_request['Cellule']
-    Variable = data_request['Variable']
-    Croisement = data_request['Croisement']
-    Zone = data_request['Zone']
-    
-    dataset_version = Croisement['JeuDonnees']['@code']
-    dataset_name = Croisement['JeuDonnees']['Source']
-    data_date = Croisement['JeuDonnees']['Annee']
-    geo_date = Zone['Millesime']['@annee']
-    codegeo_label = Zone['Millesime']['Nccenr']
-    
-    list_data = []
-    
-    for i in range(len(Cellule)):
-        dico = {**Cellule[i]['Zone'], **Cellule[i]['Mesure']}
-        modalite = Cellule[i]['Modalite']
+        codegeo = geocodes[cdg]
         
-        for m in range(len(modalite)):
-            dico_added = {modalite[m]['@variable']: modalite[m]['@code']}
-            dico = {**dico, **dico_added}
+        if type(variables) == list:
+                variables = _paste(variables, collapse = '-')
+                
+        count_sep = variables.count('-')
+        modalite = '.all' * (count_sep+1)
+        
+        link = 'https://api.insee.fr/donnees-locales/V0.1/donnees/'
+        link = link + 'geo-{}@{}/{}-{}{}'.format(variables, dataset, geo, codegeo, modalite)
+        
+        request = _request_insee(api_url = link, file_format = 'application/json;charset=utf-8')
+               
+        try:           
+                    
+            data_request = request.json()
             
-        dico['OBS_VALUE'] =  Cellule[i]['Valeur']
-        df = pd.DataFrame(dico, index=[0])
-        list_data.append(df)
-        
-    data = pd.concat(list_data)
+            #if 'Cellule' in list(data_request.keys()):
+            Cellule = data_request['Cellule']
+            Variable = data_request['Variable']
+            Croisement = data_request['Croisement']
+            Zone = data_request['Zone']
             
-    for i in range(len(Variable)):
-        df = pd.DataFrame(Variable[i]['Modalite'])
-        var_name = Variable[i]['@code']
-        df = df[['@code', 'Libelle']]
-        df.columns = [var_name, var_name + '_label']
-        data = data.merge(df, on = var_name, how = 'left')
-    
-    data = data.assign(DATASET_VERSION = dataset_version,
-                       DATASET_NAME = dataset_name,
-                       DATA_DATE = data_date,
-                       GEO_DATE = geo_date,
-                       CODEGEO_LABEL = codegeo_label)
-    
-    data.rename(columns={'@codgeo':'CODEGEO',
-                         '@nivgeo':'NIVGEO',
-                         '@code':'UNIT',
-                         '$':'UNIT_label'}, inplace=True)
-    
-    data['OBS_VALUE'] = pd.to_numeric(data['OBS_VALUE'])
-    data['DATA_DATE'] = pd.to_numeric(data['DATA_DATE'])
-    data['GEO_DATE'] = pd.to_numeric(data['GEO_DATE'])
-     
-    return(data)
+            dataset_version = Croisement['JeuDonnees']['@code']
+            dataset_name = Croisement['JeuDonnees']['Source']
+            data_date = Croisement['JeuDonnees']['Annee']
+            geo_date = Zone['Millesime']['@annee']
+            codegeo_label = Zone['Millesime']['Nccenr']
+            
+            list_data = []
+            
+            for i in range(len(Cellule)):
+                dico = {**Cellule[i]['Zone'], **Cellule[i]['Mesure']}
+                modalite = Cellule[i]['Modalite']
+                
+                for m in range(len(modalite)):
+                    dico_added = {modalite[m]['@variable']: modalite[m]['@code']}
+                    dico = {**dico, **dico_added}
+                    
+                dico['OBS_VALUE'] =  Cellule[i]['Valeur']
+                df = pd.DataFrame(dico, index=[0])
+                list_data.append(df)
+                
+            data = pd.concat(list_data)
+                    
+            for i in range(len(Variable)):
+                df = pd.DataFrame(Variable[i]['Modalite'])
+                var_name = Variable[i]['@code']
+                df = df[['@code', 'Libelle']]
+                df.columns = [var_name, var_name + '_label']
+                data = data.merge(df, on = var_name, how = 'left')
+            
+            data = data.assign(DATASET_VERSION = dataset_version,
+                               DATASET_NAME = dataset_name,
+                               DATA_DATE = data_date,
+                               GEO_DATE = geo_date,
+                               CODEGEO_LABEL = codegeo_label)
+            
+            data.rename(columns={'@codgeo':'CODEGEO',
+                                 '@nivgeo':'NIVGEO',
+                                 '@code':'UNIT',
+                                 '$':'UNIT_label'}, inplace=True)
+            
+            data['OBS_VALUE'] = pd.to_numeric(data['OBS_VALUE'])
+            data['DATA_DATE'] = pd.to_numeric(data['DATA_DATE'])
+            data['GEO_DATE'] = pd.to_numeric(data['GEO_DATE'])
+            list_data_all.append(data)
+        except:
+            data = pd.DataFrame({'CODEGEO':codegeo,'OBS_VALUE':np.nan},index=[0])
+            list_data_all.append(data)
+ 
+    data_final = pd.concat(list_data_all).reset_index(drop=True)
+        
+    return(data_final)
            
