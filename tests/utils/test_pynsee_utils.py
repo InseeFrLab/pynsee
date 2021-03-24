2188a7d06ee94c6d0a07fc25803f8a3b8ba40450
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-24 17:45:54 +0100

    include back tests

--- tests/utils/test_pynsee_utils.py
+++ tests/utils/test_pynsee_utils.py
@@ -1,118 +1,118 @@
-# # -*- coding: utf-8 -*-
-# #proxy
-# proxy_file_folder = 'C:/Users/eurhope/Desktop/insee_pylib/insee_pylib'
-# proxy_file = proxy_file_folder + '/proxy.py'
-# try:
-#     f = open(proxy_file)
-#     exec(f.read())
-#     print("Proxy file executed")
-# except IOError:
-#     print("Proxy file not accessible")
+# -*- coding: utf-8 -*-
+#proxy
+proxy_file_folder = 'C:/Users/eurhope/Desktop/insee_pylib/insee_pylib'
+proxy_file = proxy_file_folder + '/proxy.py'
+try:
+    f = open(proxy_file)
+    exec(f.read())
+    print("Proxy file executed")
+except IOError:
+    print("Proxy file not accessible")
 
-# from unittest import TestCase
-# from pandas import pandas as pd
-# import geopandas as gpd
+from unittest import TestCase
+from pandas import pandas as pd
+import geopandas as gpd
 
-# import os
+import os
 
-# from pynsee.utils._get_token import _get_token
-# from pynsee.utils._get_envir_token import _get_envir_token
-# from pynsee.utils._clean_insee_folder import _clean_insee_folder
-# from pynsee.utils._request_insee import _request_insee
-# from pynsee.utils.clear_all_cache import clear_all_cache
+from pynsee.utils._get_token import _get_token
+from pynsee.utils._get_envir_token import _get_envir_token
+from pynsee.utils._clean_insee_folder import _clean_insee_folder
+from pynsee.utils._request_insee import _request_insee
+from pynsee.utils.clear_all_cache import clear_all_cache
 
-# class TestFunction(TestCase):
+class TestFunction(TestCase):
 
-#     def test_get_token(self):        
-#         token = _get_token()
-#         self.assertTrue((token is not None))
+    def test_get_token(self):        
+        token = _get_token()
+        self.assertTrue((token is not None))
     
-#     def test_request_insee_1(self):
-#         # test both api and sdmx queries fail but token is not none        
-#         sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
-#         api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
+    def test_request_insee_1(self):
+        # test both api and sdmx queries fail but token is not none        
+        sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
+        api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
        
-#         def request_insee_test(sdmx_url=sdmx_url, api_url=api_url):
-#             _request_insee(sdmx_url=sdmx_url, api_url=api_url)
+        def request_insee_test(sdmx_url=sdmx_url, api_url=api_url):
+            _request_insee(sdmx_url=sdmx_url, api_url=api_url)
 
-#         self.assertRaises(ValueError, request_insee_test)
+        self.assertRaises(ValueError, request_insee_test)
 
-#     def test_request_insee_2(self):
-#         # if credentials are not well provided but sdmx url works
-#         _get_token.cache_clear()
-#         _get_envir_token.cache_clear() 
+    def test_request_insee_2(self):
+        # if credentials are not well provided but sdmx url works
+        _get_token.cache_clear()
+        _get_envir_token.cache_clear() 
 
-#         os.environ['insee_token'] = "test"
-#         os.environ['insee_key'] = "key"
-#         os.environ['insee_secret'] = "secret"
-#         sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/001688370"
-#         api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/001688370"
+        os.environ['insee_token'] = "test"
+        os.environ['insee_key'] = "key"
+        os.environ['insee_secret'] = "secret"
+        sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/001688370"
+        api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/001688370"
 
-#         results = _request_insee(api_url=api_url, sdmx_url=sdmx_url)
-#         test = (results.status_code == 200)
-#         self.assertTrue(test)
+        results = _request_insee(api_url=api_url, sdmx_url=sdmx_url)
+        test = (results.status_code == 200)
+        self.assertTrue(test)
     
-#     def test_request_insee_3(self):
-#         # token is none and sdmx query fails
-#         _get_token.cache_clear()
-#         _get_envir_token.cache_clear() 
+    def test_request_insee_3(self):
+        # token is none and sdmx query fails
+        _get_token.cache_clear()
+        _get_envir_token.cache_clear() 
 
-#         os.environ['insee_token'] = "test"
-#         os.environ['insee_key'] = "key"
-#         os.environ['insee_secret'] = "secret"
-#         sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
-#         api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
+        os.environ['insee_token'] = "test"
+        os.environ['insee_key'] = "key"
+        os.environ['insee_secret'] = "secret"
+        sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
+        api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
 
-#         def request_insee_test(sdmx_url=sdmx_url, api_url=api_url):
-#             _request_insee(sdmx_url=sdmx_url, api_url=api_url)
+        def request_insee_test(sdmx_url=sdmx_url, api_url=api_url):
+            _request_insee(sdmx_url=sdmx_url, api_url=api_url)
 
-#         self.assertRaises(ValueError, request_insee_test)   
+        self.assertRaises(ValueError, request_insee_test)   
     
-#     def test_request_insee_4(self):
-#         # token is none and sdmx query is None
-#         _get_token.cache_clear()
-#         _get_envir_token.cache_clear() 
+    def test_request_insee_4(self):
+        # token is none and sdmx query is None
+        _get_token.cache_clear()
+        _get_envir_token.cache_clear() 
 
-#         os.environ['insee_token'] = "test"
-#         os.environ['insee_key'] = "key"
-#         os.environ['insee_secret'] = "secret"
-#         api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
+        os.environ['insee_token'] = "test"
+        os.environ['insee_key'] = "key"
+        os.environ['insee_secret'] = "secret"
+        api_url = "https://api.insee.fr/series/BDM/V1/data/SERIES_BDM/test"
 
-#         def request_insee_test(sdmx_url=None, api_url=api_url):
-#             _request_insee(sdmx_url=sdmx_url, api_url=api_url)
+        def request_insee_test(sdmx_url=None, api_url=api_url):
+            _request_insee(sdmx_url=sdmx_url, api_url=api_url)
 
-#         self.assertRaises(ValueError, request_insee_test)
+        self.assertRaises(ValueError, request_insee_test)
     
-#     def test_request_insee_5(self):
-#         # api query is none and sdmx query fails 
-#         sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
+    def test_request_insee_5(self):
+        # api query is none and sdmx query fails 
+        sdmx_url = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM/test"
 
-#         def request_insee_test(sdmx_url=sdmx_url, api_url=None):
-#             _request_insee(sdmx_url=sdmx_url, api_url=api_url)
+        def request_insee_test(sdmx_url=sdmx_url, api_url=None):
+            _request_insee(sdmx_url=sdmx_url, api_url=api_url)
 
-#         self.assertRaises(ValueError, request_insee_test)
+        self.assertRaises(ValueError, request_insee_test)
     
-#     def test_request_insee_7(self):
-#         resp = _request_insee(api_url='https://api.insee.fr/metadonnees/V1/geo/arrondissement/01/descendants?type=arrondissement')
-#         test = (resp.status_code==404)
-#         self.assertTrue(test)       
+    def test_request_insee_7(self):
+        resp = _request_insee(api_url='https://api.insee.fr/metadonnees/V1/geo/arrondissement/01/descendants?type=arrondissement')
+        test = (resp.status_code==404)
+        self.assertTrue(test)       
         
 
-#     def test_get_envir_token(self):
-#         # if credentials are not well provided but sdmx url works   
-#         _get_envir_token.cache_clear()    
-#         os.environ['insee_token'] = "a"    
-#         token = _get_envir_token()  
-#         test = (token is None)  
-#         self.assertTrue(test)
+    def test_get_envir_token(self):
+        # if credentials are not well provided but sdmx url works   
+        _get_envir_token.cache_clear()    
+        os.environ['insee_token'] = "a"    
+        token = _get_envir_token()  
+        test = (token is None)  
+        self.assertTrue(test)
 
-#     def test_clear_all_cache(self):
-#         test = True
-#         try:
-#             clear_all_cache()
-#         except:
-#             test = False
-#         self.assertTrue(test)
+    def test_clear_all_cache(self):
+        test = True
+        try:
+            clear_all_cache()
+        except:
+            test = False
+        self.assertTrue(test)
             
         
         
