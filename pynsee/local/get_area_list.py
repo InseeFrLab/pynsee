b24c1245ea695fe6ddb91bd31e61f8e2e69e6bb6
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-03-25 13:31:00 +0100

    get all areas by default

--- pynsee/local/get_area_list.py
+++ pynsee/local/get_area_list.py
@@ -3,7 +3,7 @@
 from functools import lru_cache
 
 @lru_cache(maxsize=None)
-def get_area_list(area):    
+def get_area_list(area=None):    
     
     import pandas as pd
     from pynsee.utils._request_insee import _request_insee
@@ -12,21 +12,24 @@ def get_area_list(area):
     list_available_area = ['zonesDEmploi2020', 'airesDAttractionDesVilles2020', 'unitesUrbaines2020']
     area_string = _paste(list_available_area, collapse = " ")
     
-    if not area in list_available_area:
-        msg = "!!! {} is not available\nPlease choose area among:\n{}".format(area, area_string)
-        raise ValueError(msg)
-    
-    api_url = 'https://api.insee.fr/metadonnees/V1/geo/' + area + '?date=*'
-    
-    request = _request_insee(api_url = api_url,  file_format = 'application/json')
-    
-    data = request.json()
+    if area is not None:
+        list_available_area = [area]
+        if not area in list_available_area:
+            msg = "!!! {} is not available\nPlease choose area among:\n{}".format(area, area_string)
+            raise ValueError(msg)        
     
     list_data = []
     
-    for i in range(len(data)):
-        df = pd.DataFrame(data[i], index=[0])
-        list_data.append(df)
+    for a in list_available_area:
+        api_url = 'https://api.insee.fr/metadonnees/V1/geo/' + a + '?date=*'
+        
+        request = _request_insee(api_url = api_url,  file_format = 'application/json')
+        
+        data = request.json() 
+        
+        for i in range(len(data)):
+            df = pd.DataFrame(data[i], index=[0])
+            list_data.append(df)
     
     data_all = pd.concat(list_data).reset_index(drop=True)
     
