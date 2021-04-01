9ee3c23834375a6cd038de2d969dfa1cdc9c9ca9
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-04-01 12:30:30 +0200

    bug fix - change dir in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 import os
 import sys
 #import rinoh
-sys.path.insert(0, os.path.abspath('../'))
+os.chdir('../')
+sys.path.insert(0, os.path.abspath('./'))
 
 
 # -- Project information -----------------------------------------------------
