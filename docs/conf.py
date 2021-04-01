e4456e50d431d8467d1b50bbc3eeca5741f43653
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-04-01 14:43:23 +0200

    doc - explicit requirements from yml

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 import sphinx_copybutton
+import sphinx_rtd_theme
+
 #import rinoh
 os.chdir('../')
 sys.path.insert(0, os.path.abspath('./'))
@@ -36,7 +38,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
+
 extensions = [
      "sphinx_rtd_theme",     
      'sphinx.ext.autodoc',
