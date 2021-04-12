a3abd51ff566768e8f916e33faa71e35c400f92f
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-04-12 10:58:00 +0200

    readme rst in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
