425130a55d7c9a7157a349668fbc1c04ea69637f
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-05-12 18:02:26 +0200

    sirene metadata on employee and street

---
+++ pynsee/sirene/_employee_metadata.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+def _employee_metadata(kind='siren'):
+    
+    df = pd.DataFrame({
+            'trancheEffectifsUniteLegale':
+                ['00', '01', '02', '03', '11', '12', '21', '22',
+                 '31', '32', '41', '42', '51', '52', '53'],
+                                  
+             'effectifsMinUniteLegale' : [0,1,3,6,10,20,50,100,200,
+                                          250,500,1000,2000,5000,10000],
+                                          
+             'effectifsMaxUniteLegale' : [0,2,5,9,19,49,99,199,249,
+                                          499,999,1999,4999,9999,None]
+            })
+    
+    if kind == 'siret':
+        df.columns = ['trancheEffectifsEtablissement',
+                      'effectifsMinEtablissement',
+                      'effectifsMaxEtablissement']
+    
+    df = df.reset_index(drop=True)
+    
+    return()
---
+++ pynsee/sirene/_street_metadata.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+def _street_metadata():
+    
+    df = pd.DataFrame({
+            'typeVoieEtablissement':
+                ['AIRE','ALL', 'AV', 'BASE', 'BD',
+                 'CAMI','CAR', 'CHE',
+                 'CHEM', 'CHS', 'CITE', 'CLOS',
+                 'COIN', 'COR', 'COTE', 'COUR', 'CRS',
+                 'DOM', 'DSC', 'ECA', 'ESP',
+                 'FG', 'GARE', 'GR', 'HAM', 'HLE',
+                 'ILOT', 'IMP', 'LD', 'LOT',
+                 'MAR', 'MTE', 'PARC', 'PAS', 'PL',
+                 'PLAN', 'PLN', 'PLT', 'PONT', 'PORT',
+                 'PRO', 'PRV', 'QUA', 'RES', 'RLE',
+                 'ROC', 'RPT', 'RTE', 'RUE', 'SENTE',
+                 'SQ', 'TOUR', 'TPL', 'TRA', 'VLA',
+                 'VLGE', 'VOIE', 'ZA', 'ZAC',
+                 'ZAD', 'ZI', 'ZONE', ' '],
+            'typeVoieEtablissementLibelle':
+                ['Aire','Allée', 'Avenue', 'Base','Boulevard',
+                 'Cami','Carrefour', 'Chemin',
+                 'Cheminement', 'Chaussée', 'Cité', 'Clos',
+                 'Coin', 'Corniche', 'Cote', 'Cour', 'Cours',
+                 'Domaine', 'Descente', 'Ecart', 'Esplanade',
+                 'Faubourg', 'Gare', 'Grande Rue', 'Hameau', 'Halle',
+                 'Ilot', 'Impasse', 'Lieu dit', 'Lotissement',
+                 'Marché', 'Montée', 'Parc', 'Passage', 'Place',
+                 'Plan', 'Plaine', 'Plateau', 'Pont', 'Port',
+                 'Promenade', 'Parvis', 'Quartier', 'Résidence', 'Ruelle',
+                 'Rocade', 'Rond Point', 'Route', 'Rue', 'Sentier',
+                 'Square', 'Tour', 'Terre-Plein', 'Traverse', 'Villa',
+                 'Village', 'Voie', 'Zone artisanale', "Zone d'aménagement concerté",
+                  "Zone d'aménagement différé", 'Zone industrielle', 'Zone', ' '
+                 ]            
+            })
+    
+    return(df)
+    
