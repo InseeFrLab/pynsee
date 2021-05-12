72cb8c78351419379098d55c3b7ce8c63a1c6831
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-05-12 19:26:31 +0200

    remove try catch

--- pynsee/sirene/_clean_data.py
+++ pynsee/sirene/_clean_data.py
@@ -12,112 +12,112 @@ from pynsee.sirene._street_metadata import _street_metadata
 def _clean_data(data_final, kind = 'siren', clean=True, 
                     activity=True, legal=True, only_alive=True):            
     
-    try:
+    #try:
         # add activity metadata
-        if not data_final is None:
-            if activity:
-                naf5 = get_activity_list("NAF5")
-                naf5 = naf5[["NAF5", "TITLE_NAF5_FR"]]
-                    
-                if "activitePrincipaleUniteLegale" in data_final.columns:
-                    
-                    naf5_merge = naf5.rename(columns={"NAF5":"activitePrincipaleUniteLegale",
-                                                        "TITLE_NAF5_FR":"activitePrincipaleUniteLegaleLibelle"})
-                    data_final = data_final.merge(naf5_merge, on ="activitePrincipaleUniteLegale", how="left")
+    if not data_final is None:
+        if activity:
+            naf5 = get_activity_list("NAF5")
+            naf5 = naf5[["NAF5", "TITLE_NAF5_FR"]]
                 
-                if "activitePrincipaleEtablissement" in data_final.columns:
-                    
-                    naf5_merge = naf5.rename(columns={"NAF5":"activitePrincipaleEtablissement",
-                                                        "TITLE_NAF5_FR":"activitePrincipaleEtablissementLibelle"})
-                    data_final = data_final.merge(naf5_merge, on ="activitePrincipaleEtablissement", how="left")
+            if "activitePrincipaleUniteLegale" in data_final.columns:
                 
-                    
-            #remove companies which no longer exist
-            if only_alive:
-                if 'dateFin' in data_final.columns:
-                    data_final = data_final[data_final['dateFin'].isnull()]
+                naf5_merge = naf5.rename(columns={"NAF5":"activitePrincipaleUniteLegale",
+                                                    "TITLE_NAF5_FR":"activitePrincipaleUniteLegaleLibelle"})
+                data_final = data_final.merge(naf5_merge, on ="activitePrincipaleUniteLegale", how="left")
             
-            # add legal entities title
-            if legal:
-                if 'categorieJuridiqueUniteLegale' in data_final.columns:
-                    try:
-                        list_legal_code = data_final.categorieJuridiqueUniteLegale.unique()
-                        data_legal = get_insee_legal_entity(list_legal_code, print_err_msg=False)
-                        data_legal = data_legal[['code', 'title']]
-                        data_legal = data_legal.rename(columns={'code' : 'categorieJuridiqueUniteLegale',
-                                                                'title' : 'categorieJuridiqueUniteLegaleLibelle'})
-                        data_final = data_final.merge(data_legal, on = 'categorieJuridiqueUniteLegale', how='left')
-                    except:
-                        pass
-            # empty columns at the end
-            list_all_na_col = [col for col in data_final.columns if all(pd.isna(data_final[col]))]
-            list_first_col = [col for col in data_final.columns if col not in list_all_na_col]
-            data_final = data_final[list_first_col + list_all_na_col]
-            
-            # change colummn order        
-            if kind == 'siren' :
-                first_col = ['siren', 'denominationUniteLegale' ,
-                                'dateFin', 'dateDebut', 'categorieEntreprise',
-                                'categorieJuridiqueUniteLegale', 'activitePrincipaleUniteLegale']
+            if "activitePrincipaleEtablissement" in data_final.columns:
                 
-            if kind == 'siret' :            
-                first_col = ['siren', 'nic' , 'siret', 'dateDebut', 'dateCreationEtablissement',
-                                'dateCreationUniteLegale', 'dateFin',                          
-                                'denominationUniteLegale', 'nomUniteLegale',
-                                'prenomUsuelUniteLegale',
-                                'categorieEntreprise',
-                                'categorieJuridiqueUniteLegale',
-                                'activitePrincipaleUniteLegale',
-                                'activitePrincipaleEtablissement',
-                                'numeroVoieEtablissement',
-                                'typeVoieEtablissement',
-                                'libelleVoieEtablissement', 'codePostalEtablissement',
-                                'libelleCommuneEtablissement',  'codeCommuneEtablissement']
-                
-            if set(first_col).issubset(data_final.columns):
-                other_col = [col for col in data_final if col not in first_col]
-                data_final = data_final[first_col + other_col]
+                naf5_merge = naf5.rename(columns={"NAF5":"activitePrincipaleEtablissement",
+                                                    "TITLE_NAF5_FR":"activitePrincipaleEtablissementLibelle"})
+                data_final = data_final.merge(naf5_merge, on ="activitePrincipaleEtablissement", how="left")
             
-            # add employee range metadata
-            df_empl_siren = _employee_metadata(kind='siren')
-            df_empl_siret = _employee_metadata(kind='siret')
+                
+        #remove companies which no longer exist
+        if only_alive:
+            if 'dateFin' in data_final.columns:
+                data_final = data_final[data_final['dateFin'].isnull()]
         
-            if 'trancheEffectifsUniteLegale' in data_final.columns:
-                data_final = data_final.merge(df_empl_siren, on ='trancheEffectifsUniteLegale', how='left')
+        # add legal entities title
+        if legal:
+            if 'categorieJuridiqueUniteLegale' in data_final.columns:
+                try:
+                    list_legal_code = data_final.categorieJuridiqueUniteLegale.unique()
+                    data_legal = get_insee_legal_entity(list_legal_code, print_err_msg=False)
+                    data_legal = data_legal[['code', 'title']]
+                    data_legal = data_legal.rename(columns={'code' : 'categorieJuridiqueUniteLegale',
+                                                            'title' : 'categorieJuridiqueUniteLegaleLibelle'})
+                    data_final = data_final.merge(data_legal, on = 'categorieJuridiqueUniteLegale', how='left')
+                except:
+                    pass
+        # empty columns at the end
+        list_all_na_col = [col for col in data_final.columns if all(pd.isna(data_final[col]))]
+        list_first_col = [col for col in data_final.columns if col not in list_all_na_col]
+        data_final = data_final[list_first_col + list_all_na_col]
+        
+        # change colummn order        
+        if kind == 'siren' :
+            first_col = ['siren', 'denominationUniteLegale' ,
+                            'dateFin', 'dateDebut', 'categorieEntreprise',
+                            'categorieJuridiqueUniteLegale', 'activitePrincipaleUniteLegale']
             
-            if 'trancheEffectifsEtablissement' in data_final.columns:
-                data_final = data_final.merge(df_empl_siret, on ='trancheEffectifsEtablissement', how='left')
+        if kind == 'siret' :            
+            first_col = ['siren', 'nic' , 'siret', 'dateDebut', 'dateCreationEtablissement',
+                            'dateCreationUniteLegale', 'dateFin',                          
+                            'denominationUniteLegale', 'nomUniteLegale',
+                            'prenomUsuelUniteLegale',
+                            'categorieEntreprise',
+                            'categorieJuridiqueUniteLegale',
+                            'activitePrincipaleUniteLegale',
+                            'activitePrincipaleEtablissement',
+                            'numeroVoieEtablissement',
+                            'typeVoieEtablissement',
+                            'libelleVoieEtablissement', 'codePostalEtablissement',
+                            'libelleCommuneEtablissement',  'codeCommuneEtablissement']
             
-            # add street metadata
-            df_street = _street_metadata()
-            if 'typeVoieEtablissement' in data_final.columns:
-                data_final = data_final.merge(df_street, on ='typeVoieEtablissement', how='left')
-            
-            def _move_col_after(df, col, col_ref):
-                if col in df.columns:
-                    if col_ref in df.columns:
-                        loc_var = df.columns.get_loc(col_ref)
-                        col2insert = df[col]
-                        df = df.drop([col], axis = 1)
-                        df.insert(loc_var+1, col, col2insert)
-                return(df)
+        if set(first_col).issubset(data_final.columns):
+            other_col = [col for col in data_final if col not in first_col]
+            data_final = data_final[first_col + other_col]
+        
+        # add employee range metadata
+        df_empl_siren = _employee_metadata(kind='siren')
+        df_empl_siret = _employee_metadata(kind='siret')
+    
+        if 'trancheEffectifsUniteLegale' in data_final.columns:
+            data_final = data_final.merge(df_empl_siren, on ='trancheEffectifsUniteLegale', how='left')
+        
+        if 'trancheEffectifsEtablissement' in data_final.columns:
+            data_final = data_final.merge(df_empl_siret, on ='trancheEffectifsEtablissement', how='left')
+        
+        # add street metadata
+        df_street = _street_metadata()
+        if 'typeVoieEtablissement' in data_final.columns:
+            data_final = data_final.merge(df_street, on ='typeVoieEtablissement', how='left')
+        
+        def _move_col_after(df, col, col_ref):
+            if col in df.columns:
+                if col_ref in df.columns:
+                    loc_var = df.columns.get_loc(col_ref)
+                    col2insert = df[col]
+                    df = df.drop([col], axis = 1)
+                    df.insert(loc_var+1, col, col2insert)
+            return(df)
 
-            # move columns title after columns containing values
-            for var in ['categorieJuridiqueUniteLegale', 
-                        'activitePrincipaleUniteLegale',
-                        'activitePrincipaleEtablissement',
-                        'typeVoieEtablissement']:
-                
-                data_final = _move_col_after(data_final, var + 'Libelle', var)
+        # move columns title after columns containing values
+        for var in ['categorieJuridiqueUniteLegale', 
+                    'activitePrincipaleUniteLegale',
+                    'activitePrincipaleEtablissement',
+                    'typeVoieEtablissement']:
             
-            for k in ['Etablissement', 'UniteLegale']:
-                data_final = _move_col_after(data_final, 'effectifsMax' + k, 'trancheEffectifs' + k)
-                data_final = _move_col_after(data_final, 'effectifsMin' + k, 'trancheEffectifs' + k)
-                    
-            if clean:
-                data_final = data_final.dropna(axis=1, how='all')
-    except:
+            data_final = _move_col_after(data_final, var + 'Libelle', var)
+        
+        for k in ['Etablissement', 'UniteLegale']:
+            data_final = _move_col_after(data_final, 'effectifsMax' + k, 'trancheEffectifs' + k)
+            data_final = _move_col_after(data_final, 'effectifsMin' + k, 'trancheEffectifs' + k)
+                
+        if clean:
+            data_final = data_final.dropna(axis=1, how='all')
+    #except:
         #print('\n!!! Error : Data cleaning and harmonization failed !!!')
-        raise ValueError('\n!!! Error : Data cleaning and harmonization failed !!!')  
+        #raise ValueError('\n!!! Error : Data cleaning and harmonization failed !!!')  
 
     return(data_final)
