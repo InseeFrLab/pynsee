2bdd5d27b861ccde3296c5f3038c76fc8c703416
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-06-07 13:13:42 +0200

    function missing in metadata init

--- pynsee/metadata/__init__.py
+++ pynsee/metadata/__init__.py
@@ -1,5 +1,5 @@
 
 from .get_definition_list import get_definition_list
-from .get_insee_definition import get_insee_definition
+from .get_definition import get_definition
 from .get_activity_list import get_activity_list
 from .get_insee_legal_entity import get_insee_legal_entity
