12476ecafdea87b9d07d3e0ab6f2d5b9c79d882c
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-06-15 14:26:12 +0200

    import internal package data function change

--- pynsee/macrodata/_get_idbank_internal_data.py
+++ pynsee/macrodata/_get_idbank_internal_data.py
@@ -12,7 +12,7 @@ from pynsee.utils._hash import _hash
 #from functools import lru_cache
 
 #@lru_cache(maxsize=None)
-def _get_idbank_internal_data():
+def _get_idbank_internal_data(update=False):
     
     insee_folder = _create_insee_folder()
     
@@ -20,13 +20,16 @@ def _get_idbank_internal_data():
     data_final_file = insee_folder + '/' + _hash('idbank_list_internal_final')
     zip_file = pkg_resources.resource_stream(__name__, 'data/idbank_list_internal.zip')
     
-    if not os.path.exists(data_final_file):
+    if (not os.path.exists(data_final_file)) | update:
        
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(insee_folder)            
             
-        idbank_list = pd.read_csv(data_file, encoding = 'latin-1',
-                                  quotechar='"', sep=',', dtype=str, usecols = [0,1,2,538,539])
+        # idbank_list = pd.read_csv(data_file, encoding = 'latin-1',
+        #                           quotechar='"', sep=',', dtype=str, usecols = [0,1,2,538,539])
+
+        idbank_list = pd.read_csv(data_file, encoding = 'utf-8',
+                                  quotechar='"', sep=',', dtype=str)
         
         os.remove(data_file)
         idbank_list.to_pickle(data_final_file)
