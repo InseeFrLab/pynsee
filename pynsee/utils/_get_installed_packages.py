d1a492440404deda8dcf10e52489355a02db22d8
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-06-16 15:33:18 +0200

    get installed packages

---
+++ pynsee/utils/_get_installed_packages.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright : INSEE, 2021
+
+import pkg_resources
+import pandas as pd
+
+def _get_installed_packages():
+    
+    installed_packages = pkg_resources.working_set
+        
+    df = pd.DataFrame({"package": None, "version": None}, index=[0])
+    j = 0
+    for i in installed_packages:
+        df.loc[j, "package"], df.loc[j, "version"] = i.key, i.version
+        j += 1
+    
+    
+    return(df)
