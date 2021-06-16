88b6ce26d045bb79af476a38203ba4c2bf09155f
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-06-16 14:19:12 +0200

    minor doc

--- pynsee/metadata/get_activity_list.py
+++ pynsee/metadata/get_activity_list.py
@@ -27,7 +27,7 @@ def get_activity_list(level, version='NAFRev2'):
     Args:
         level (str): Levels available are :  A10, A21, A38, A64, A88, A129, A138, NAF1, NAF2, NAF3, NAF4, NAF5
         
-        version (str, optional): Defaults to 'latest'.
+        version (str, optional): Defaults to 'NAFRev2'.
 
     Raises:
         ValueError: an error is raised if level is not in the default list
