2e79c4ea70b2835c296f5abf7536d889d6c5d9cd
hadrilec <leclerc.hadrien@gmail.com>
2022-01-25 10:13:25 +0000

    separate tests with and withtout polygon on get_geodata function

--- tests/geodata/test_pynsee_geodata.py
+++ tests/geodata/test_pynsee_geodata.py
@@ -24,10 +24,6 @@ class TestFunction(TestCase):
         list_geom_type = []
         # ident = 'LIMITES_ADMINISTRATIVES_EXPRESS.LATEST:epci'
         # ident = 'ADMINEXPRESS-COG-CARTO.LATEST:commune'
-
-        dep29 = get_geodata(id='ADMINEXPRESS-COG-CARTO.LATEST:departement', update=True)
-        dep29 = dep29[dep29["insee_dep"] == "29"]
-        geodep29 = dep29.get_geom()
        
         for id in range(len(ids)):
             
@@ -35,23 +31,44 @@ class TestFunction(TestCase):
             print("%s %s" % (id, ident))
 
             data = get_geodata(id=ident, update=True)
-            
-            geom = data.get_geom()
-            list_geom_type += [type(geom)]
 
-            data = get_geodata(id=ident, update=True)            
-            geom = data.get_geom()
-            list_geom_type += [type(geom)]
+            if type(datapoly) == GeoDataframe:    
+                geom = data.get_geom()
+                list_geom_type += [type(geom)]
 
-            datapoly = get_geodata(id=ident, update=True, polygon=geom)   
-            if type(datapoly) == GeoDataframe:        
-                geompoly = datapoly.get_geom()
-                list_geom_type += [type(geompoly)]
+        print(list_geom_type)            
+        
+        test = all([typegeo in [Polygon, MultiPolygon, MultiLineString, MultiPoint] for typegeo in list_geom_type])
+
+        self.assertTrue(test)
+
+    def test_get_geodata_poly(self):
+
+        df = get_geodata_list(update=True)
+        ids = df.Identifier.to_list()
+
+        list_geom_type = []
+        # ident = 'LIMITES_ADMINISTRATIVES_EXPRESS.LATEST:epci'
+        # ident = 'ADMINEXPRESS-COG-CARTO.LATEST:commune'
+
+        dep29 = get_geodata(id='ADMINEXPRESS-COG-CARTO.LATEST:departement', update=True)
+        dep29 = dep29[dep29["insee_dep"] == "29"]
+        geodep29 = dep29.get_geom()
+
+        for id in range(len(ids)):
+        
+                ident = ids[id]
+                print("%s %s" % (id, ident))
+
+                datapoly = get_geodata(id=ident, update=True, polygon=geom) 
+
+                if type(datapoly) == GeoDataframe:        
+                        geompoly = datapoly.get_geom()
+                        list_geom_type += [type(geompoly)]
 
         print(list_geom_type)            
 
-        # test with a localisation constraint
-        
+        # test with a localisation constraint        
         test = all([typegeo in [Polygon, MultiPolygon, MultiLineString, MultiPoint] for typegeo in list_geom_type])
 
         self.assertTrue(test)
