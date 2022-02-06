4d8712d876fe606ce777d21615894f2678cbe58b
Hadrien Leclerc <66560093+hadrilec@users.noreply.github.com>
2022-02-06 01:06:09 +0100

    Update __init__.py

--- pynsee/localdata/__init__.py
+++ pynsee/localdata/__init__.py
@@ -2,8 +2,6 @@
 
 from .get_area_list import get_area_list
 from .get_geo_list import get_geo_list
-from .get_map_list import get_map_list
-from .get_map_link import get_map_link
 
 from .get_local_data import get_local_data
 from .get_included_area import get_included_area
@@ -13,6 +11,6 @@ from .get_population import get_population
 from .get_old_city import get_old_city
 from .get_new_city import get_new_city
 
-__all__ = ['get_area_list', 'get_geo_list', 'get_map_list', 'get_map_link',
+__all__ = ['get_area_list', 'get_geo_list', 
            'get_local_data', 'get_included_area', 'get_nivgeo_list', 'get_local_metadata',
            'get_population', 'get_old_city', 'get_new_city']
