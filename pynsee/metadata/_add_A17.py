cc724633453c73631ccb867e56ebae548d336447
hadrilec <leclerc.hadrien@gmail.com>
2022-02-14 16:45:51 +0000

    move_col_after in separated file

--- pynsee/metadata/_add_A17.py
+++ pynsee/metadata/_add_A17.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+from pynsee.utils._move_col_after import _move_col_after
 
 def _add_A17(df):
 
@@ -33,15 +34,6 @@ def _add_A17(df):
     df2["A17"] = df2["A17"].replace(to_replace ='^O|^P|^Q', value = 'OQ', regex = True)
 
     df2["A17"] = df2["A17"].replace(to_replace ='^R|^S|^T|^U', value = 'RU', regex = True)
-
-    def _move_col_after(df, col, col_ref):
-            if col in df.columns:
-                if col_ref in df.columns:
-                    loc_var = df.columns.get_loc(col_ref)
-                    col2insert = df[col]
-                    df = df.drop([col], axis=1)
-                    df.insert(loc_var + 1, col, col2insert)
-            return(df)
 
     df2 = _move_col_after(df2, "A17", "A10")
 
