9f141f35d23b634be2447e8058a1390c15eb1298
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2022-02-18 17:34:20 +0100

    test fix

--- tests/geodata/test_pynsee_geodata.py
+++ tests/geodata/test_pynsee_geodata.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 import pandas as pd
 import sys
 
-from shapely.geometry import Point, Polygon, MultiPolygon, MultiLineString, MultiPoint
+from shapely.geometry import Polygon, MultiPolygon, MultiLineString, MultiPoint
 
 from pynsee.geodata.get_geodata_list import get_geodata_list
 from pynsee.geodata.get_geodata import get_geodata
@@ -21,16 +21,13 @@ class TestFunction(TestCase):
             df = get_geodata_list(update=True)
             self.assertTrue(isinstance(df, pd.DataFrame))
 
-            ids = df.Identifier.to_list()
-
-            list_geom_type = []
-
             data = get_geodata(id='ADMINEXPRESS-COG-CARTO.LATEST:commune', update=True) 
             self.assertTrue(isinstance(data, GeoDataframe))
             geo = data.get_geom()
             self.assertTrue(isinstance(geo, MultiPolygon))
 
             dep29 = get_geodata(id='ADMINEXPRESS-COG-CARTO.LATEST:departement', update=True)
+            dep29 = dep29[dep29["insee_dep"] == "29"]
             self.assertTrue(isinstance(dep29, GeoDataframe))
             geo29 = dep29.get_geom()
             self.assertTrue(isinstance(geo29, MultiPolygon))
