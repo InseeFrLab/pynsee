b60683dcbc4d43aea5fd1f08d5d7e1af8fb237f5
hadrilec <leclerc.hadrien@gmail.com>
2022-02-18 14:45:55 +0000

    print rm

--- pynsee/geodata/_geojson_parser.py
+++ pynsee/geodata/_geojson_parser.py
@@ -8,41 +8,39 @@ import warnings
 
 def _geojson_parser(data):
         
-data_list = []
-list_shapes = []
+    data_list = []
+    list_shapes = []
 
-for c in range(len(data)):
-
-    print(c) 
+    for c in range(len(data)):
+            
+        df = data[c]['properties']
         
-    df = data[c]['properties']
-    
-    df2 = pd.DataFrame({f : df[f] for f in df.keys()\
-                        if f not in (['geometry', 'bbox']) and
-                        (type(df[f]) is not list)}, index=[0])
-    
-    for k in df.keys():
-        dfk = df[k]
-        if type(dfk) is list:
-            try:
-                df2[k] = [df[k]]
-            except:
-                pass        
+        df2 = pd.DataFrame({f : df[f] for f in df.keys()\
+                            if f not in (['geometry', 'bbox']) and
+                            (type(df[f]) is not list)}, index=[0])
+        
+        for k in df.keys():
+            dfk = df[k]
+            if type(dfk) is list:
+                try:
+                    df2[k] = [df[k]]
+                except:
+                    pass        
 
 
-    geom = data[c]['geometry']['coordinates']
-    
-    data_type = data[c]['geometry']['type']
-    
-    Shape = shape({"type": data_type, "coordinates": geom})
-    # list_shapes = [Shape.geoms[x] for x in range(len(Shape.geoms))]
+        geom = data[c]['geometry']['coordinates']
+        
+        data_type = data[c]['geometry']['type']
+        
+        Shape = shape({"type": data_type, "coordinates": geom})
+        # list_shapes = [Shape.geoms[x] for x in range(len(Shape.geoms))]
 
-    if data_type in ['MultiLineString','MultiPolygon', 'MultiPoint','LineString', 'Polygon', 'Point']:
-        list_shapes += [Shape]
-        data_list.append(df2)
-    else:            
-        print(f"Unsupported shape {data_type} has been removed")
-                
+        if data_type in ['MultiLineString','MultiPolygon', 'MultiPoint','LineString', 'Polygon', 'Point']:
+            list_shapes += [Shape]
+            data_list.append(df2)
+        else:            
+            print(f"Unsupported shape {data_type} has been removed")
+                    
 
     data_all = pd.concat(data_list)
     
