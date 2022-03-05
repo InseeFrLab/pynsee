2072cd6fb6d47bc8426f45753e2c69020e9a9f67
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2022-03-05 14:13:13 +0100

    try zoom by dep area

--- pynsee/geodata/_rescale_geom.py
+++ pynsee/geodata/_rescale_geom.py
@@ -2,9 +2,12 @@
 from shapely.affinity import scale
 from shapely.geometry import MultiPolygon
 
-def _rescale_geom(geo, factor=1):
+def _rescale_geom(geo, factor=1, center=None):
     
     list_geoms = []
+    
+    if center is None:
+        center = "center"    
 
     for i in range(len(geo.geoms)):
 
@@ -13,6 +16,6 @@ def _rescale_geom(geo, factor=1):
             xfact= factor,
             yfact= factor,
             zfact=1.0,
-            origin="center")]
+            origin=center)]
 
     return MultiPolygon(list_geoms) 
--- pynsee/geodata/zoom.py
+++ pynsee/geodata/zoom.py
@@ -6,35 +6,44 @@ from shapely.geometry import Point, LineString
 
 from pynsee.geodata._extract_bounds import _extract_bounds
 from pynsee.geodata._rescale_geom import _rescale_geom
+from pynsee.geodata._get_center import _get_center
 
 def zoom(self, 
         departement = ['75','92', '93','94'], #'91', '78', '77', '95'
         center = (-133583.39, 5971815.98),
         radius = 650000,
         angle = math.pi * (1 - 3.5 * 1/9),
-        factor = 0.1):
+        factor = 2):
 
     df = self
 
     if all([x in df.columns for x in ['insee_dep', 'geometry']]):
         
-        zoomedDep = df[df['insee_dep'].isin(departement)]        
-        zoomedDep = zoomedDep.reset_index(drop=True)
-        zoomedDepGeo = zoomedDep.get_geom()
+        zoomDeps = df[df['insee_dep'].isin(departement)]        
+        zoomDeps = zoomDeps.reset_index(drop=True)
+        zoomDepsGeo = zoomDeps.get_geom()
+        centerZoom = _get_center(zoomDepsGeo)
         
-        zoomedDepGeo = _rescale_geom(geo = zoomedDepGeo, factor = factor)
+        list_dep_scaled = []
         
-        end = Point(center[0] + radius , center[1] )
-        line = LineString([center, end])
-        line = rotate(line, angle, origin=center, use_radians=True)
-        endPoint = Point(line.coords[1])
-                
-        maxxdep = max(_extract_bounds(zoomedDepGeo, var= 'maxx'))
-        maxydep = max(_extract_bounds(zoomedDepGeo, var= 'maxy'))
+        for d in departement:
+            zoomDep = df[df['insee_dep'] == d].reset_index(drop=True)
         
-        xoff = endPoint.coords.xy[0][0] - maxxdep 
-        yoff = endPoint.coords.xy[1][0] - maxydep 
-        zoomedDep['geometry'] = zoomedDep['geometry'].apply(lambda x: trs(x, xoff=xoff, yoff=yoff))
+            zoomDepGeo = zoomDep.get_geom()
+            zoomDepGeo = _rescale_geom(geo = zoomDepGeo, factor = factor, center=centerZoom)
+            
+            end = Point(center[0] + radius, center[1])
+            line = LineString([center, end])
+            line = rotate(line, angle, origin=center, use_radians=True)
+            endPoint = Point(line.coords[1])
+                    
+            maxxdep = max(_extract_bounds(zoomedDepGeo, var= 'maxx'))
+            maxydep = max(_extract_bounds(zoomedDepGeo, var= 'maxy'))
+            
+            xoff = endPoint.coords.xy[0][0] - maxxdep 
+            yoff = endPoint.coords.xy[1][0] - maxydep 
+        
+        #zoomedDep['geometry'] = zoomedDep['geometry'].apply(lambda x: trs(x, xoff=xoff, yoff=yoff))
 
         self = pd.concat([self, zoomedDep])                
 
