308bfa85ea8d7e46f031111c90149962901342aa
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2022-03-11 19:36:28 +0100

    convert_polygon save - useful

---
+++ pynsee/geodata/_convert_polygon.py
@@ -0,0 +1,14 @@
+
+
+def _convert_polygon(geo, crs_in='EPSG:4326', crs_out='EPSG:3857'):
+
+    import pyproj
+    from shapely.ops import transform
+    
+    crsIn = pyproj.CRS(crs_in)
+    crsOut = pyproj.CRS(crs_out)
+
+    project = pyproj.Transformer.from_crs(crsIn, crsOut, always_xy=True).transform
+    geo_converted = transform(project, geo)
+
+    return geo_converted
