bb7d50dce10ef438db907d520103037ceec50c1d
hadrilec <leclerc.hadrien@gmail.com>
2022-03-14 21:44:01 +0000

    bug fix file format for sirene queries + more messages in init_conn

--- pynsee/utils/init_conn.py
+++ pynsee/utils/init_conn.py
@@ -28,6 +28,8 @@ def init_conn(insee_key, insee_secret, proxy_server=""):
     d = {'credentials':{'insee_key': insee_key,
                   'insee_secret': insee_secret,
                   'proxy_server': proxy_server}}
+
+    #os.environ['pynsee_query_print'] = 'True'
     
     home = str(Path.home())
     
@@ -49,13 +51,8 @@ def init_conn(insee_key, insee_secret, proxy_server=""):
     if token is None:
         print('!!! Token is missing, please check insee_key and insee_secret are correct !!!')
     else:
-        print(f'Your token has been created : {token}')
-    
-    file_format='application/xml'
-
-    headers = {'Accept': file_format,
-                'Authorization': 'Bearer ' + token}
-    
+        print(f'Token has been created : {token}')
+        
     try:
         proxies = {'http': os.environ['http_proxy'],
                    'https': os.environ['http_proxy']}
@@ -64,15 +61,27 @@ def init_conn(insee_key, insee_secret, proxy_server=""):
 
     queries = ['https://api.insee.fr/series/BDM/V1/dataflow/FR1/all',
                 'https://api.insee.fr/metadonnees/V1/codes/cj/n3/5599',
-                'https://api.insee.fr/entreprises/sirene/V3/siren/552081317', 
+                'https://api.insee.fr/entreprises/sirene/V3/siret?q=activitePrincipaleUniteLegale:86.10*&nombre=1000', 
                 'https://api.insee.fr/donnees-locales/V0.1/donnees/geo-SEXE-DIPL_19@GEO2020RP2017/FE-1.all.all']
     apis = ['BDM', 'Metadata', 'Sirene', 'Local Data']
 
+    file_format=['application/xml','application/xml', 'application/json;charset=utf-8', 'application/xml']
+
+    list_requests_status = []
+
     for q in range(len(queries)):
-    
+
+        headers = {'Accept': file_format[q],
+                'Authorization': 'Bearer ' + token}    
         api_url = queries[q]
+
         _wait_api_query_limit(api_url)
         results = requests.get(api_url, proxies=proxies, headers=headers)
+
         if results.status_code != 200:
             print('!!! Please subscribe to {} API on api.insee.fr !!!'.format(apis[q]))
+        list_requests_status += [results.status_code]
+
+    if all([sts == 200 for sts in list_requests_status]):
+        print("Subscription to all INSEE's APIs has been successfull")
             
