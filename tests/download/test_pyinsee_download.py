b5f1a8a54d77100b244ba5e937ae066a11c33493
hadrilec <leclerc.hadrien@gmail.com>
2022-06-09 14:51:43 +0000

    tests dowload module new functions

--- tests/download/test_pyinsee_download.py
+++ tests/download/test_pyinsee_download.py
@@ -5,44 +5,61 @@ import hashlib
 import pandas as pd
 
 from pynsee.download import *
-from pynsee.download import info_data
-from pynsee.download import telechargerDonnees
-from pynsee.download import load_data
-from pynsee.download import check_year_available
-from pynsee.download import dict_data_source
-from pynsee.download import millesimesDisponibles
-from pynsee.download import telechargerFichier
-
+#from pynsee.download import info_data
+#from pynsee.download import telechargerDonnees
+#from pynsee.download import load_data
+#from pynsee.download import check_year_available
+#from pynsee.download import dict_data_source
+#from pynsee.download import millesimesDisponibles
+#from pynsee.download import telechargerFichier
+from pynsee.download import download_file
+from pynsee.download import get_file_list
 
 class MyTests(unittest.TestCase):
-
+    
+    def test_get_file_list(self):
+        df = get_file_list()
+        self.assertTrue(isinstance(df, pd.DataFrame))
+    
+    def test_download_file(self):
+        meta = get_file_list()
+        list_file = list(meta.id)        
+        list_file_check = list_file[:100] + list_file[-100:]
+        
+        for f in list_file_check:
+            df = download_file(f)
+        
+            self.assertTrue(isinstance(df, pd.DataFrame))
+            self.assertTrue((len(df.columns) > 2))
+            
+            
     # info_data ---------------------------------
         
-    def test_levensthein_error_typo(self):
-        with self.assertRaises(ValueError):
-            info_data("SIRENE_SIRET_NONDIFg")
+    #def test_levensthein_error_typo(self):
+    #    with self.assertRaises(ValueError):
+    #        info_data("SIRENE_SIRET_NONDIFg")
 
-    def test_levensthein_error_big_typo(self):
-        with self.assertRaises(ValueError):
-            info_data("randomword")
+    #def test_levensthein_error_big_typo(self):
+    #    with self.assertRaises(ValueError):
+    #        info_data("randomword")
 
-    def test_info_no_date(self):
-        with self.assertRaises(ValueError):
-            info_data("FILOSOFI_REG")
+    #def test_info_no_date(self):
+    #    with self.assertRaises(ValueError):
+    #        info_data("FILOSOFI_REG")
 
 
 
-    def test_date_info_data_dernier(self):
-        self.assertIsInstance(info_data("FILOSOFI_REG", date = "dernier"), dict)
+    #def test_date_info_data_dernier(self):
+    #    self.assertIsInstance(info_data("FILOSOFI_REG", date = "dernier"), dict)
 
-    def test_date_info_data_latest(self):
-        self.assertIsInstance(info_data("FILOSOFI_REG", date = "latest"), dict)
+    #def test_date_info_data_latest(self):
+    #    self.assertIsInstance(info_data("FILOSOFI_REG", date = "latest"), dict)
 
-    def test_date_info_data_str(self):
-        self.assertIsInstance(info_data("FILOSOFI_REG", date = "2016"), dict)
+    #def test_date_info_data_str(self):
+    #    self.assertIsInstance(info_data("FILOSOFI_REG", date = "2016"), dict)
 
-    def test_date_info_data_int(self):
-        self.assertIsInstance(info_data("FILOSOFI_REG", date = 2016), dict)
+    #def test_date_info_data_int(self):
+    #    self.assertIsInstance(info_data("FILOSOFI_REG", date = 2016), dict)
 
     # def test_nodate(self):
     #   self.assertIsInstance(info_data("SIRENE_SIRET_NONDIFF"), dict)
@@ -50,15 +67,15 @@ class MyTests(unittest.TestCase):
 
     # check_year_available -------------------------
 
-    def test_error_check_year_available_typo(self):
-        with self.assertRaises(ValueError):
-            check_year_available("randomword")
+    #def test_error_check_year_available_typo(self):
+    #    with self.assertRaises(ValueError):
+    #        check_year_available("randomword")
 
-    def test_deprecation_millesimesDisponibles(self):
-        with warnings.catch_warnings(record=True) as ws:
-            warnings.simplefilter("always", DeprecationWarning)
-            millesimesDisponibles("FILOSOFI_COM")
-        self.assertEqual(len(ws), 1)
+    #def test_deprecation_millesimesDisponibles(self):
+    #    with warnings.catch_warnings(record=True) as ws:
+    #        warnings.simplefilter("always", DeprecationWarning)
+    #        millesimesDisponibles("FILOSOFI_COM")
+    #    self.assertEqual(len(ws), 1)
 
             
 
@@ -69,10 +86,10 @@ class MyTests(unittest.TestCase):
 
     # download_store_file ----------------------------
 
-    def test_error_multiple_data_no_year(self):
-        with self.assertRaises(ValueError):
-            download_store_file("FILOSOFI_REG")        
-
+    #def test_error_multiple_data_no_year(self):
+    #    with self.assertRaises(ValueError):
+    #        download_store_file("FILOSOFI_REG")        
+    """"
     def test_year_string(self):
         filosofi_data = download_store_file("FILOSOFI_REG", date = "2016")
         self.assertIsInstance(filosofi_data, dict)
@@ -119,13 +136,13 @@ class MyTests(unittest.TestCase):
         #self.assertTrue(os.path.isfile(path_unzipped))
         self.assertEqual(filosofi_data["result"]['fichier_donnees'], path_unzipped.split("/")[-1])
         self.assertEqual(hashlib.md5(open(path_zipped, 'rb').read()).hexdigest(), filosofi_data['result']['md5'])
+    """
 
-
-    def test_deprecation_telechargerFichier(self):
-        with warnings.catch_warnings(record=True) as ws:
-            warnings.simplefilter("always", DeprecationWarning)
-            telechargerFichier("FILOSOFI_REG", date = "latest")
-        self.assertEqual(len(ws), 1)
+    #def test_deprecation_telechargerFichier(self):
+    #    with warnings.catch_warnings(record=True) as ws:
+    #        warnings.simplefilter("always", DeprecationWarning)
+    #        telechargerFichier("FILOSOFI_REG", date = "latest")
+    #    self.assertEqual(len(ws), 1)
 
 
     # telechargerDonnees ----------------------------
@@ -150,14 +167,14 @@ class MyTests(unittest.TestCase):
     #     df = load_data("AIRE_URBAINE", date = "latest")
     #     self.assertIsInstance(df, pd.DataFrame)
     
-    def test_deprecation_notice_telechargerDonnees(self):
-        with self.assertWarns(Warning):
-            telechargerDonnees("AIRE_URBAINE", date = "latest")      
+    #def test_deprecation_notice_telechargerDonnees(self):
+    #    with self.assertWarns(Warning):
+    #        telechargerDonnees("AIRE_URBAINE", date = "latest")      
 
-    def test_equivalence_loaddata_telechargerDonnees(self):
-        df1 = telechargerDonnees("AIRE_URBAINE", date = "latest")  
-        df2 = load_data("AIRE_URBAINE", date = "latest")
-        pd.testing.assert_frame_equal(df1,df2)
+    #def test_equivalence_loaddata_telechargerDonnees(self):
+    #    df1 = telechargerDonnees("AIRE_URBAINE", date = "latest")  
+    #    df2 = load_data("AIRE_URBAINE", date = "latest")
+    #    pd.testing.assert_frame_equal(df1,df2)
 
         
 if __name__ == '__main__':
