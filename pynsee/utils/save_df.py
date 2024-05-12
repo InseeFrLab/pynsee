4567d1db67e7b519f9a9f81f4c7ef95fc132480b
Hadrien Leclerc <66560093+hadrilec@users.noreply.github.com>
2024-05-12 23:55:28 +0200

    Update pynsee/utils/save_df.py
    Co-authored-by: tfardet <79037344+tfardet@users.noreply.github.com>

--- pynsee/utils/save_df.py
+++ pynsee/utils/save_df.py
@@ -46,7 +46,7 @@ def save_df(obj=pd.DataFrame, parquet=True, day_lapse_max=None):
                                     
             update = kwargs.get("update", False)
 
-            silent = any((a == 'silent') and (kwargs[a] is True) for a in kwargs)
+            silent = kwargs.get("silence", False)
             
             if os.path.exists(file_name):
                 file_date_last_modif = datetime.datetime.fromtimestamp(
