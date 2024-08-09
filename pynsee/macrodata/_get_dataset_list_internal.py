aa00db508a947282dc8fa8cf4837de5bc30d03be
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2024-08-09 09:43:24 +0200

    Remove unnecessary tempdir from _get_dataset_list_internal

--- pynsee/macrodata/_get_dataset_list_internal.py
+++ pynsee/macrodata/_get_dataset_list_internal.py
@@ -1,10 +1,9 @@
+import io
 import zipfile
 import pkg_resources
 import pandas as pd
-import os
 
 from pynsee.utils.save_df import save_df
-from pynsee.utils._get_temp_dir import _get_temp_dir
 
 
 @save_df(day_lapse_max=90)
@@ -14,11 +13,8 @@ def _get_dataset_list_internal():
         __name__, "data/dataset_list_internal.zip"
     )
 
-    temp_folder = _get_temp_dir()
-    dataset_file = os.path.join(temp_folder, "dataset_list_internal.csv")
-
     with zipfile.ZipFile(zip_file, "r") as zip_ref:
-        zip_ref.extractall(temp_folder)
+        dataset_file = io.BytesIO(zip_ref.read("dataset_list_internal.csv"))
 
     dataset_list = pd.read_csv(
         dataset_file, encoding="utf-8", quotechar='"', sep=",", dtype=str
@@ -27,11 +23,5 @@ def _get_dataset_list_internal():
     col = "Unnamed: 0"
     if col in dataset_list.columns:
         dataset_list = dataset_list.drop(columns={col})
-
-    # Nota: do not remove dirpath, _get_temp_dir is managed through lru_cache
-    try:
-        os.remove(dataset_file)
-    except FileNotFoundError:
-        pass
 
     return dataset_list
