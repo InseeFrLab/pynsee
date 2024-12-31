9df5e2a35aba181b845f5608f1bcada3e29a372d
hadrilec <leclerc.hadrien@gmail.com>
2024-12-31 19:50:39 +0000

    minor - hidden print in a back function

--- pynsee/geodata/_find_wfs_closest_match.py
+++ pynsee/geodata/_find_wfs_closest_match.py
@@ -7,11 +7,18 @@ from pynsee.geodata.get_geodata_list import get_geodata_list
 
 string = "ADMINEXPRESS-COG.LATEST:departement"
 
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+
 def _find_wfs_closest_match(string=string):
-    sys.stdout = open(os.devnull, 'w')
-    wfs = get_geodata_list()
-    sys.stdout = sys.__stdout__
-    
+    with HiddenPrints():
+        wfs = get_geodata_list()    
 
     list_sugg = list(wfs.Identifier.unique()) 
     suggestions = difflib.get_close_matches(
