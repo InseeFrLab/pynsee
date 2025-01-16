5f9c16f1797e11873a7664bf567902a605c46527
thomas.grandjean <tgrandje@gmail.com>
2025-01-16 15:22:33 +0100

    move staticmethods to run on py3.9

--- pynsee/utils/requests_params.py
+++ pynsee/utils/requests_params.py
@@ -36,6 +36,34 @@ def _get_requests_proxies():
     return proxies
 
 
+def after_log(
+    logger: "logging.Logger",
+    log_level: int,
+    sec_format: str = "%0.3f",
+):
+    "Log every retry."
+
+    def log_it(retry_state):
+        try:
+            retry_state.outcome.result()
+        except Exception:
+            logger.log(
+                log_level,
+                f"{tenacity._utils.to_ordinal(retry_state.attempt_number)}"
+                f" request call to with args={retry_state.args[1:]} "
+                f"after {sec_format % retry_state.seconds_since_start}s.",
+            )
+
+    return log_it
+
+
+def traceback_after_retries(retry_state):
+    logger.error(
+        "An error happened to request call with" f"args={retry_state.args[1:]}"
+    )
+    return retry_state.outcome.result()
+
+
 class PynseeAPISession(requests.Session):
     """
     Session class used to allow specific timeouts and
@@ -51,35 +79,6 @@ class PynseeAPISession(requests.Session):
         self.mount("https://", adapter)
         self.headers.update(_get_requests_proxies())
         self.headers.update(_get_requests_headers())
-
-    @staticmethod
-    def after_log(
-        logger: "logging.Logger",
-        log_level: int,
-        sec_format: str = "%0.3f",
-    ):
-        "Log every retry."
-
-        def log_it(retry_state):
-            try:
-                retry_state.outcome.result()
-            except Exception:
-                logger.log(
-                    log_level,
-                    f"{tenacity._utils.to_ordinal(retry_state.attempt_number)}"
-                    f" request call to with args={retry_state.args[1:]} "
-                    f"after {sec_format % retry_state.seconds_since_start}s.",
-                )
-
-        return log_it
-
-    @staticmethod
-    def traceback_after_retries(retry_state):
-        logger.error(
-            "An error happened to request call with"
-            f"args={retry_state.args[1:]}"
-        )
-        return retry_state.outcome.result()
 
     @retry(
         reraise=True,
