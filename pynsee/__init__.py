a0426212ba6cad53c578f1525e0cd831b932be20
thomas.grandjean <tgrandje@gmail.com>
2025-01-17 17:13:39 +0100

    set hard coded version

--- pynsee/__init__.py
+++ pynsee/__init__.py
@@ -6,6 +6,4 @@ from pynsee.metadata import *
 from pynsee.utils import *
 from pynsee.download import *
 
-from importlib_metadata import version
-
-__version__ = version(__package__)
+__version__ = "0.2.0"
