fec0647f3ee44413e652a1523c471cd9ab32976f
Tanguy Fardet <tanguyfardet@protonmail.com>
2025-02-12 11:29:49 +0100

    Correct zoom in notebook, correct get_geom alias

--- tests/geodata/test_pynsee_geodata.py
+++ tests/geodata/test_pynsee_geodata.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pytest
 from geopandas import GeoSeries
 from requests import RequestException
-from shapely.geometry import Polygon, MultiPolygon, Point
+from shapely.geometry import Polygon, MultiPolygon, MultiPoint, Point
 
 from pynsee.geodata import GeoFrDataFrame
 from pynsee.geodata.get_geodata import get_geodata
@@ -110,7 +110,7 @@ def test_get_geodata_overseas():
 
     with pytest.warns(DeprecationWarning):
         geo = chflieu.get_geom()
-        assert isinstance(geo, GeoSeries)
+        assert isinstance(geo, MultiPoint)
 
     geo_chflieu = chflieu.transform_overseas().zoom().geometry
     assert isinstance(geo_chflieu, GeoSeries)
