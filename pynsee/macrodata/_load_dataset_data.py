b8d8346f6785a832fd706398acc4c6d1cb38dfac
hadrilec <leclerc.hadrien@gmail.com>
2025-05-27 21:34:30 +0000

    metadata only from the series datasets not from all datasets + inflation example fix

--- pynsee/macrodata/_load_dataset_data.py
+++ pynsee/macrodata/_load_dataset_data.py
@@ -7,9 +7,13 @@ from ._get_metadata import _get_dataset_metadata
 
 
 @save_df(day_lapse_max=90)
-def _load_dataset_data(update=False, silent=False):
+def _load_dataset_data(datasets=None, update=False, silent=False):
 
     list_dataset = list(get_dataset_list(silent=silent).id.unique())
+    
+    if datasets is not None:
+        list_dataset = [d for d in list_dataset if d in datasets]
+        
     list_metadata = []
 
     for dt in trange(len(list_dataset), desc="Metadata download"):
