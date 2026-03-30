16659cb811c45f456790648bb50c78177f1878e3
Thomas Grandjean <tgrandje@gmail.com>
2026-03-30 13:26:44 +0200

    feat: check geodata availability before queries (#298)
    Leverage existing functions to check geodatasets availability before queries (current failing process is slow & doesn't show any hints on what went wrong).

--- pynsee/geodata/_find_wfs_closest_match.py
+++ pynsee/geodata/_find_wfs_closest_match.py
@@ -6,10 +6,27 @@ from pynsee.utils.HiddenPrints import HiddenPrints
 string = "ADMINEXPRESS-COG.LATEST:departement"
 
 
-def _find_wfs_closest_match(string=string):
+def _find_wfs_closest_match(string: str = string, limit: int = 1) -> list:
+    """
+    Find the X closest matches among available WFS datasets.
+
+    Parameters
+    ----------
+    string : str, optional
+        Dataset identifier to match. The default is ADMINEXPRESS-COG.LATEST:departement.
+    limit : int, optional
+        Maximum number of datasets to retrieve. The default is 1.
+
+    Returns
+    -------
+    list
+        List of potential matches.
+
+    """
+
     with HiddenPrints():
         wfs = get_geodata_list()
 
     list_sugg = list(wfs.Identifier.unique())
     suggestions = difflib.get_close_matches(string, list_sugg)
-    return suggestions[0]
+    return suggestions[:limit]
--- pynsee/geodata/_get_geodata_with_backup.py
+++ pynsee/geodata/_get_geodata_with_backup.py
@@ -1,20 +1,24 @@
-from requests import RequestException
+import logging
 
 from ._find_wfs_closest_match import _find_wfs_closest_match
+from .get_geodata_list import get_geodata_list
 from ._get_geodata import _get_geodata
 from .geofrdataframe import GeoFrDataFrame
 
+logger = logging.getLogger(__name__)
+
 
 def _get_geodata_with_backup(string: str) -> GeoFrDataFrame:
-    try:
-        df = _get_geodata(string, ignore_error=False)
-    except RequestException as e:
-        res = e.response
 
-        if hasattr(res, "status_code") and res.status_code == 400:
-            str_replacement = _find_wfs_closest_match(string)
-            df = _get_geodata(str_replacement)
-        else:
-            raise e
+    # check if dataset is available to ensure faster backup
+    dsets = get_geodata_list()
+    if string not in set(dsets.Identifier):
+        str_replacement = _find_wfs_closest_match(string)[0]
+        logger.warning(
+            "%s not found, switching to %s instead", string, str_replacement
+        )
+        string = str_replacement
+
+    df = _get_geodata(string)
 
     return df
